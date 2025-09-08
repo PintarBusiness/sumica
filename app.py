65ee57e73e61b612572e3e7ec7a5dbff73f4c460
PintarBusiness <160147958+PintarBusiness@users.noreply.github.com>
2025-09-08 19:38:50 +0200

    Dodelana prva stran index, začetek druge strani index, začetek o nas

--- app.py
+++ app.py
@@ -6,5 +6,30 @@ app = Flask(__name__)
 def domov():
     return render_template("index.html")
 
+@app.route("/onas")
+def onas():
+    return render_template("onas.html")
+
+@app.route("/ponudbe")
+def ponudbe():
+    return render_template("ponudbe.html")
+
+@app.route("/galerija")
+def galerija():
+    return render_template("galerija.html")
+
+@app.route("/kontakt")
+def kontakt():
+    return render_template("kontakt.html")
+
+@app.route("/lokacija")
+def lokacija():
+    return render_template("lokacija.html")
+
+@app.route("/rezervacija")
+def rezervacija():
+    return render_template("rezervacija.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
